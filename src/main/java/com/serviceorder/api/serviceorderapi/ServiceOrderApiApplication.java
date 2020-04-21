7cc7a4e28a93aba696bec9b091041b84a1b87d52
Fernand <fernando.vgarcia@hotmail.com>
2020-04-20 21:35:26 -0300

    Spring project initialized.

---
+++ src/main/java/com/serviceorder/api/serviceorderapi/ServiceOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.serviceorder.api.serviceorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviceorder/api/serviceorderapi/ServiceOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceorder.api.serviceorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
